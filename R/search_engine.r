b4a295f9dcc33025becef26edbc78c09ae160f03
daftmath <daftmath@gmail.com>
2016-07-11 13:04:37 -0500

    changed code to reference functions, so the entire qdap and tm packages don't have to be loaded.

--- R/search_engine.r
+++ R/search_engine.r
@@ -5,31 +5,31 @@
 orion_prep <- function(docs) {
 
   # Stemming, removing punctuation, white spaces to improve matching ability
-  corp <- VectorSource(docs) %>%
-    Corpus() %>%
+  corp <- tm::VectorSource(docs) %>%
+    tm::Corpus() %>%
     suppressWarnings() %>%
-    tm_map(., removePunctuation) %>%
-    tm_map(., stemDocument) %>%
-    tm_map(., tolower) %>%
-    tm_map(., stripWhitespace)
+    tm::tm_map(., removePunctuation) %>%
+    tm::tm_map(., stemDocument) %>%
+    tm::tm_map(., tolower) %>%
+    tm::tm_map(., stripWhitespace)
 
-  corp <- tm_map(corp, PlainTextDocument)
+  corp <- tm::tm_map(corp, PlainTextDocument)
 
-  tdm <- TermDocumentMatrix(corp) %>% weightTfIdf()
+  tdm <- tm::TermDocumentMatrix(corp) %>% tm::weightTfIdf()
 
   return(as.matrix(tdm))
 }
 
 
 # Search Engine
-orion <- function(tdm, search_q, print_q=FALSE) {
+orion_search <- function(tdm, search_q, print_q=FALSE) {
 
   # Preparing the search query
   search_q2 <- unlist(strsplit(search_q, split=" "))
 
   # Locating synonyms
   for (i in 1:length(search_q2)) {
-    search_q3 <- append(search_q2, synonyms(search_q2[i], return.list = FALSE))
+    search_q3 <- append(search_q2, qdap::synonyms(search_q2[i], return.list = FALSE))
   }
   search_q4 <- unlist(strsplit(search_q3, split=" "))
   search_q5 <- paste(search_q3, collapse = " ")
@@ -38,15 +38,15 @@ orion <- function(tdm, search_q, print_q=FALSE) {
     print(search_q5)
   }
 
-  query_clean <- VectorSource(search_q5) %>%
-    Corpus() %>%
+  query_clean <- tm::VectorSource(search_q5) %>%
+    tm::Corpus() %>%
     suppressWarnings() %>%
-    tm_map(., removePunctuation) %>%
-    tm_map(., stemDocument) %>%
-    tm_map(., tolower) %>%
-    tm_map(., stripWhitespace)
+    tm::tm_map(., removePunctuation) %>%
+    tm::tm_map(., stemDocument) %>%
+    tm::tm_map(., tolower) %>%
+    tm::tm_map(., stripWhitespace)
 
-  search_q6 <-  tm_map(query_clean, PlainTextDocument) %>% TermDocumentMatrix() %>% as.matrix()
+  search_q6 <-  tm::tm_map(query_clean, PlainTextDocument) %>% tm::TermDocumentMatrix() %>% as.matrix()
 
   # Creating vector to compare against weighted tdm.
   query_vec <- rep(0, NROW(tdm))
@@ -66,7 +66,7 @@ orion_time_test <- function(n, sp_test) {
   lst = list()
   for (i in 1:n) {
     ptm <- proc.time()
-    test <- suppressMessages(orion(sp_test, "nutrition stowage"))
+    test <- suppressMessages(orion_search(sp_test, "nutrition stowage"))
     time_dur <- proc.time() - ptm
     time_dur <- time_dur %>% .[3] %>% as.numeric()
     lst <- c(lst, time_dur)
